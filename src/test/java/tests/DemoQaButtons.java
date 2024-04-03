18c84ba3a52599a935c6f58536249c9cb51ec3c5
Uğur Kaan Yeşil <ugurkan.yesil@gmail.com>
2024-04-03 22:20:37 +0300

    first commit and add buttons hw

---
+++ src/test/java/tests/DemoQaButtons.java
@@ -0,0 +1,26 @@
+package tests;
+
+import com.codeborne.selenide.SelenideElement;
+import org.testng.annotations.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
+public class DemoQaButtons {
+    @Test
+    public void printDynamicMessageTest() {
+        open("https://demoqa.com/elements");
+        assertThat("https://demoqa.com/elements");
+
+        SelenideElement buttonsButton = $(byId("item-4"));
+        buttonsButton.click();
+        assertThat("https://demoqa.com/buttons");
+
+        SelenideElement clickMeButton = $(byCssSelector(".col-md-6 div:nth-of-type(3) > .btn"));
+        clickMeButton.scrollTo().click();
+        System.out.println($(byId("dynamicClickMessage")).getText());
+    }
+
+    @AfterTest
+    public void tearDown() {
+    }
+}
